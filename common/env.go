730f0d41b07e45985096997c099cf03dcadf4184
jokyo02 <149650929+jokyo02@users.noreply.github.com>
2024-01-16 15:51:10 +0800

    跟随v1.18.7

--- common/env.go
+++ common/env.go
@@ -53,7 +53,13 @@ func initEnv() {
 	BypassServer = os.Getenv("BYPASS_SERVER")
 
 	BingBaseUrl = os.Getenv("BING_BASE_URL")
+	if os.Getenv("BING_PROXY_DM") != "" {
+		BingBaseUrl = os.Getenv("BING_PROXY_DM")
+	}
 	SydneyBaseUrl = os.Getenv("SYDNEY_BASE_URL")
+	if os.Getenv("SYDNEY_PROXY_DM") != "" {
+		SydneyBaseUrl = os.Getenv("SYDNEY_PROXY_DM")
+	}
 	if BingBaseUrl != "" {
 		BING_URL, _ = url.Parse(BingBaseUrl)
 	}
