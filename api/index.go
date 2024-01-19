88a2e292951acb1823e6053d7d6ebad47226551f
jokyo02 <149650929+jokyo02@users.noreply.github.com>
2024-01-19 21:40:09 +0800

    跟随v1.18.10

--- api/index.go
+++ api/index.go
@@ -5,7 +5,12 @@ import (
 	"adams549659584/go-proxy-bingai/common"
 	"net/http"
 	"strings"
+	"os"
+	"net/url"
 )
+
+var BingURL = os.Getenv("BING_PROXY_DM")
+
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
@@ -17,7 +22,12 @@ func Index(w http.ResponseWriter, r *http.Request) {
 			helper.UnauthorizedResult(w)
 			return
 		}
+	if BingURL == "" {
+		BingURL = common.BING_URL.String()
 	}
+	proxyurl, _ := url.Parse(BingURL)
+		common.NewSingleHostReverseProxy(proxyurl).ServeHTTP(w, r)
+	} else {
 	common.NewSingleHostReverseProxy(common.BING_URL).ServeHTTP(w, r)
-
+	}
 }
