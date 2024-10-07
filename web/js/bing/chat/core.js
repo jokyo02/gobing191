6a5f35fabfe81617ee4f7e3c5ff80ca8c18617f4
jokyo02 <149650929+jokyo02@users.noreply.github.com>
2024-10-07 21:10:06 +0800

    Update core.js

--- web/js/bing/chat/core.js
+++ web/js/bing/chat/core.js
@@ -10,15 +10,15 @@
 }
 )(_w.onload, _w.si_PP);
 _w.rms.js(
-  { 'A:rms:answers:Shared:BingCore.Bundle': '/rp/a224OTR91R7nhnUp3RpvwJI8dVU.br.js' },
-  //{ 'A:rms:answers:Shared:BingCore.Bundle': '/rp/IvfoLO3XuUzsCyfv53CD-ckZU3s.br.js' },
-  //{ 'A:rms:answers:Web:FreeSydneyHelper': '\/rp\/cfhgV3_HFyB3URSxigzE0QzgsF8.br.js' },
-  //{ 'A:rms:answers:Web:SydneyFSCHelper': '/rp/UHSe4HZxjUmY8afun89YHML74eQ.br.js' },
-  { 'A:rms:answers:VisualSystem:ConversationScope': '/rp/hcAzGBxfE9oRVAA1rPm7ah7lmms.br.js' },
- //{ 'A:rms:answers:CodexBundle:cib-bundle': '/rp/_lytZzaZiTlZINgpNKZ-D0qMH_o.br.js' },
-  { 'A:rms:answers:CodexBundle:cib-bundle': '/rp/6JYDi9-2XVe6p7GjQcvfkzdAi_0.br.js' },
-  { 'A:rms:answers:SharedStaticAssets:speech-sdk': '/rp/6slp3E-BqFf904Cz6cCWPY1bh9E.br.js' },
+  { 'A:rms:answers:Shared:BingCore.Bundle': '/rp/nAb2_uMWSHAhScje1ODrwe4cVoA.br.js' },
+  // { 'A:rms:answers:Web:FreeSydneyHelper': '/rp/cfhgV3_HFyB3URSxigzE0QzgsF8.br.js' },
+  // { 'A:rms:answers:Web:SydneyFSCHelper': '/rp/wJbTArpXX7iY8vTObyrSJ0XZMCE.br.js' },
+  { 'A:rms:answers:VisualSystem:ConversationScope': '/rp/ascyKFaOzJnUTZ_D-Mv819BZ0Go.br.js' },
+  { 'A:rms:answers:CodexBundle:cib-bundle': '/rp/V6IKxmcE5rJ2bGlG-v3sIvR-550.br.js' },
+  { 'A:rms:answers:SharedStaticAssets:speech-sdk': '/rp/bll21ZO27j3KPE27uQBxt24c2Fw.br.js' },
   { 'A:rms:answers:Web:SydneyWelcomeScreenBase':'/rp/KgDPK_mKWjMgWe6o0SKA9dfE00Y.br.js' },
   { 'A:rms:answers:Web:SydneyWelcomeScreen':'/rp/sNzL8vfHHNmyqYOSQey9fNYd3kI.br.js' },
-  { 'A:rms:answers:Web:SydneyFullScreenConv': '/rp/yuhncmz_MT-3TSF9a5y0wgIL43U.gz.js'},
-  );
+  { 'A:rms:answers:Web:SydneyFullScreenConv': '/rp/QGOwWXVSYoKX5FtgJZTIy7dpusc.br.js' },
+  // { 'A:rms:answers:Web:ChatHomeScreenBase': '/rp/SlXkn2s3KjsnNrD80OtYBr8qECA.br.js' },
+  // { 'A:rms:answers:Web:ChatHomeScreen': '/rp/x05EBLDKo9Gp0rV1W9OmmpoFp0o.br.js' },
+);
